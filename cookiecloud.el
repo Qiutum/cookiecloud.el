1671c844d332dfaa08e2807aa92d4974d6e652f5
Qiutum <zh4710jj@gmail.com>
2025-07-16 10:11:49 +0800

    First init

---
+++ cookiecloud.el
@@ -0,0 +1,25 @@
+;;; cookiecloud.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Qiutum
+;;
+;; Author: Qiutum <zh4710jj@gmail.com>
+;; Maintainer: Qiutum <zh4710jj@gmail.com>
+;; Created: July 16, 2025
+;; Modified: July 16, 2025
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc
+;; Homepage: https://github.com/arch/cookiecloud
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'cookiecloud)
+;;; cookiecloud.el ends here
